5d58f458f3f1d8f10e372658c44b919bc08f015a
ArmenZz <sebi_saske@yahoo.com>
2019-12-22 05:52:50 +0200

    v0.4.3
    -fixed signup issue

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -45,7 +45,31 @@ class _LoginState extends State<Login> {
       }
   }
 
+  signup(User user, AuthNotifier authNotifier) async {
+    AuthResult authResult = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: user.email, password: user.password)
+        .catchError((error) => print(error.code));
 
+    if (authResult != null) {
+      UserUpdateInfo updateInfo = UserUpdateInfo();
+      updateInfo.displayName = user.displayName;
+
+      FirebaseUser firebaseUser = authResult.user;
+
+      if (firebaseUser != null) {
+        await firebaseUser.updateProfile(updateInfo);
+
+        await firebaseUser.reload();
+
+        print("Sign up: $firebaseUser");
+
+        FirebaseUser currentUser = await FirebaseAuth.instance.currentUser();
+        authNotifier.setUser(currentUser);
+      }
+    }else{
+      setState(() => loading = false);
+    }
+  }
 
 
   @override
